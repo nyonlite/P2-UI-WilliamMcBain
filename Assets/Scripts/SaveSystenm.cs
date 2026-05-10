df9bfcb993cf0426a7069f44f3ff69ad7773f05c
TachiBanaGQ <141469342+TachiBanaGQ@users.noreply.github.com>
2026-05-10 19:41:19 -0400

    save feature
    close

--- Assets/Scripts/SaveSystenm.cs
+++ Assets/Scripts/SaveSystenm.cs
@@ -1,13 +1,38 @@
 using System.IO;
-using UnityEngine;
 using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
 
-public static class SaveSystenm 
+public static class SaveSystenm
 {
- public static void SavePlayer (PlayerController player)
+    public static void SavePlayer(PlayerController player)
     {
         BinaryFormatter formatter = new BinaryFormatter();
-        string path = Application.persistentDataPath + "player fun";
-        //the
+        string path = Application.persistentDataPath + "/player.fun";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        PlayerData data = new PlayerData(player);
+
+        formatter.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public static PlayerData LoadPlayer()
+    {
+        string path = Application.persistentDataPath + "/player.fun";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            stream.Close();
+
+            return data;
+        }
+        else
+        {
+            Debug.LogError("Save file not found in" + path);
+            return null;
+        }
     }
 }
