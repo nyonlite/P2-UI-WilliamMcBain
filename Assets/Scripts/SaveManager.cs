082abae41154c6e689e47fd55344705b5252e8c4
nyonlite <nyonlite@gmail.com>
2026-05-10 00:14:45 -0400

    UpdateWithBasicSave

--- Assets/Scripts/SaveManager.cs
+++ Assets/Scripts/SaveManager.cs
@@ -2,14 +2,6 @@ using System;
 using System.IO;
 using UnityEngine;
 
-// My SaveManager is a Serializable POCO. It contains the default values of the fields
-// to be saved and the filepath which itself is not saved since it's static
-// Save() simply saves this while Load() load an instance of SaveManager from json and
-// returns it (or returns a new one if the file was not found). 
-// Load() is called only a single time, by the GameManager's Start(), and its return 
-// value is the stored in a static field for global access. Once GameManager is done 
-// with its Start(), it sends an event and this is when classes that need to act on a
-// saved value do their thing.
 
 [Serializable]
 public class SaveManager
@@ -19,7 +11,7 @@ public class SaveManager
     // default values
 
     public Vector3 loadAt; // default is 0, 0, 0
-    public float timer = 0f;
+    
 
 
 
